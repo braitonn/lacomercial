29171499e0498f126abc38223583f9b0f4b66bb0
Matias Eizmendi <juaneizmendi@institutocedec.com>
2023-08-01 20:30:46 -0300

    incertar datos -parte 1 Clase 01/08

--- api/modelos.php
+++ api/modelos.php
@@ -72,6 +72,30 @@ class ModeloABM extends Modelo {
     public function set_limit() {
         return $this->limit;
     }
+    /**
+     * Metodo de insercion datos
+     * @param valores los valores a isertar
+     */
+    public function insertar($valores){
+        //INSERT INTRO articulo (codigo, nombre, descripcion, precio, imagen)
+        //VALUES ('201', 'Samsung Galaxy S20', 'Procesador:xxx', )
+        $campos = '';
+        $datos = '';
+
+        //recorrer el objeto $valores
+        foreach($valores as $key=$value){
+            $value = "'".$value."'";
+            $campos .= $key.", ";
+            $daros .= $value.", ";
+        }
+        $campos = substr($campos,0,strlen($campos)-1);
+        $datos = substr($datos,0,strlen($datos)-1);
+        
+        //intruccion SQL
+        $sql = "INSERT INTO $this->tabla($campos) VALUE($datos)";
+        echo $sql;
+        $this->_db->query($sql);
+    }
 }
 
 ?>
